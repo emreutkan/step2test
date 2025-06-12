7ebf9dc3573b77277a972603851a2524a9b5762f
Irfan Emre Utkan <127414322+emreutkan@users.noreply.github.com>
2025-06-12 19:34:57 +0300

    Update Dockerfile: change SSH port from 22 to 2222

--- init.sh
+++ init.sh
@@ -1,9 +1,3 @@
-#!/usr/bin/env bash
-set -e
-
-# Start the SSH service in the background
+#!/bin/bash
 service ssh start
-
-# Start the Uvicorn server in the foreground.
-# The script will wait here until uvicorn exits, keeping the container running.
-uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
+uvicorn app.main:app --host 0.0.0.0 --port 8000
